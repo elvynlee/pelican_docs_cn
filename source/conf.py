b6e78031b327e5d5fcaf93d177e90385a1aec994
elvyn <22519307+elvynlee@users.noreply.github.com>
2019-08-01 14:02:12 +0800

    update index

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,7 @@ author = 'Elvyn'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '4.1.0'
 
 
 # -- General configuration ---------------------------------------------------
